d613708278c842e7e066673d49d3e7a9b15fa4a1
lxcao <caolingxin@hotmail.com>
2024-07-02 13:53:51 +0800

    update code

--- translate.py
+++ translate.py
@@ -12,15 +12,18 @@ endpoint = "https://language.aiservice.us-ashburn-1.oci.oraclecloud.com"
 ai_client = oci.ai_language.AIServiceLanguageClient(oci.config.from_file(profile_name="specialist2-4sdk"),
                                                     service_endpoint=endpoint)
 
-#specify the documents for translation.
-document_details = oci.ai_language.models.BatchLanguageTranslationDetails(documents = [
-    oci.ai_language.models.TranslationDocument(key="1", source_language_code="en",
-                                               target_language_code="fr",
-                                               text="I'm gonna make him an offer he can't refuse"),
-    oci.ai_language.models.TranslationDocument(key="2", source_language_code="es",
-                                               target_language_code="en",
-                                               text="Nunca es tarde para aprender")
-])
 
-output = ai_client.batch_language_translation(batch_language_translation_details= document_details)
+key1 = "doc1"
+key2 = "doc2"
+text1 = "The Indy Autonomous Challenge is the worlds first head-to-head, high speed autonomous race taking place at the Indianapolis Motor Speedway"
+text2 = "OCI will be the cloud engine for the artificial intelligence models that drive the MIT Driverless cars."
+target_language = "de" #TODO specify the target language
+compartment_id = "ocid1.compartment.oc1..aaaaaaaagwmfwd2pdlllrvfdlkhfrg3wjuo6syt7rk7rauewkjjoe5w7crrq" #TODO Provide your compartmentId here
+
+doc1 = oci.ai_language.models.TextDocument(key=key1, text=text1, language_code="en")
+doc2 = oci.ai_language.models.TextDocument(key=key2, text=text2, language_code="en")
+documents = [doc1, doc2]
+
+batch_language_translation_details = oci.ai_language.models.BatchLanguageTranslationDetails(documents=documents, compartment_id=compartment_id, target_language_code=target_language)
+output = ai_client.batch_language_translation (batch_language_translation_details)
 print(output.data)
